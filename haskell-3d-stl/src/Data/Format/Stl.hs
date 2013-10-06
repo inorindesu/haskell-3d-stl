76408a3cbece4e29267a860ed965fed48311b143
Inori Sakura <inorindesu@gmail.com>
2013-10-07 01:36:42 +0800

    Added some function interface (skeleton).

--- haskell-3d-stl/src/Data/Format/Stl.hs
+++ haskell-3d-stl/src/Data/Format/Stl.hs
@@ -1,6 +1,18 @@
+{-|
+This module provides read / write support for STL file format.
+-}
 module Data.Format.Stl
        (
+         parseStlText,
+         toStlText
        ) where
 
 import qualified Data.Text.Lazy as LT
 import Data.Format.Stl.TextParser
+import Data.TriangleList
+
+parseStlText :: LT.Text -> Maybe Mesh
+parseStlText = undefined
+
+toStlText :: Mesh -> LT.Text
+toStlText = undefined
--- haskell-3d-stl/src/Data/Format/Stl/TextParser.hs
+++ haskell-3d-stl/src/Data/Format/Stl/TextParser.hs
@@ -1,3 +1,11 @@
 module Data.Format.Stl.TextParser
        (
+         pStl
        ) where
+
+import Data.Text.Lazy as LT
+import Data.Attoparsec.Text.Lazy
+import Data.TriangleList
+
+pStl :: Parser Mesh
+pStl = undefined
