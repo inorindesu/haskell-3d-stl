4fc5dbff14e2f19e2ced22bf8df8beea07e26968
Inori Sakura <inorindesu@gmail.com>
2013-10-07 01:36:02 +0800

    Added a 'Mesh' data type.

--- haskell-3d-stl/src/Data/TriangleList.hs
+++ haskell-3d-stl/src/Data/TriangleList.hs
@@ -8,23 +8,27 @@ module Data.TriangleList
          -- * Data format
          Vector (..),
          Triangle (..),
-         Normal (..)
+         Normal (..),
+         Mesh (..)
        ) where
 
--- | A vector is consist of x y and z coordinates in space.
-data Vector = Vector {-# UNPACK #-} !Double
-                     {-# UNPACK #-} !Double
-                     {-# UNPACK #-} !Double deriving Eq
+import Data.Vector hiding ((++))
 
-instance Show Vector where
-  show (Vector x y z) = "(" ++ show x ++ ", " ++ show y ++ ", " ++ show z ++ ")"
+-- | A vector is consist of x y and z coordinates in space.
+data Vector3D = Vector3D {-# UNPACK #-} !Double
+                         {-# UNPACK #-} !Double
+                         {-# UNPACK #-} !Double deriving Eq
+
+instance Show Vector3D where
+  show (Vector3D x y z) = "(" ++ show x ++ ", " ++ show y ++ ", " ++ show z ++ ")"
 
 -- | A triangle is consist of 3 'Vectors' and a 'Normal'
-data Triangle = Triangle Vector Vector Vector Normal deriving Eq
+data Triangle = Triangle Vector3D Vector3D Vector3D Normal deriving Eq
 
 instance Show Triangle where
   show (Triangle a b c n) = show a ++ " - " ++ show b ++ " - " ++ show c ++ " normal " ++ show n
 
--- | A normal is actuall a vector.
-type Normal = Vector
+-- | A normal is actuall a 
+type Normal = Vector3D
 
+type Mesh = Vector Triangle
