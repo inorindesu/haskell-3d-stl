85be768c047a0c1e5658b122ac79b330e58de1c4
Inori Sakura <inorindesu@gmail.com>
2013-10-09 00:57:14 +0800

    Added test skeleton

---
+++ haskell-3d-stl/src/stl-parse-test.hs
@@ -0,0 +1,9 @@
+import Data.Format.Stl
+import Data.TriangleList
+import qualified Data.Text.Lazy as LT
+import Test.QuickCheck
+
+main = undefined
+
+prop_canParseSelf :: Mesh -> Bool
+prop_canParseSelf = undefined
