91bcbde23ff1f9d2c0115439394f869cd420bec9
Inori Sakura <inorindesu@gmail.com>
2013-10-09 01:54:44 +0800

    Completed test code.
    According to test, the parser has error.

--- haskell-3d-stl/src/stl-parse-test.hs
+++ haskell-3d-stl/src/stl-parse-test.hs
@@ -1,9 +1,25 @@
 import Data.Format.Stl
 import Data.Mesh
+import qualified Data.Vector as V
 import qualified Data.Text.Lazy as LT
 import Test.QuickCheck
+import Control.Applicative
 
-main = undefined
+main = verboseCheck prop_canParseSelf
 
-prop_canParseSelf :: Mesh -> Bool
-prop_canParseSelf = undefined
+instance Arbitrary a => Arbitrary (V.Vector a) where
+  arbitrary = V.fromList <$> listOf1 arbitrary
+
+instance Arbitrary Triangle where
+  arbitrary = Triangle <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+instance Arbitrary Vector3D where
+  arbitrary = Vector3D <$> arbitrary <*> arbitrary <*> arbitrary
+
+prop_canParseSelf :: String -> Mesh -> Bool
+prop_canParseSelf name m =
+  let t = toStlText m name
+      parseResult = parseStlText t
+  in case parseResult of
+       Nothing -> False
+       Just a -> a == m
